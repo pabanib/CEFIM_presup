9731c526b9b932fcf4bed5170948d75808c23f7d
Pablo pc <pabanib@hotmail.com>
2026-03-04 13:20:44 -0300

    agrego el modelo es

--- motores/motor_clasificador.py
+++ motores/motor_clasificador.py
@@ -5,7 +5,7 @@ import sys
 from pathlib import Path
 
 # Asegurar que la clase NormalizadorPreguntas esté disponible cuando se cargue el modelo
-from motores.transformadores import NormalizadorPreguntas
+from modelos.transformadores import NormalizadorPreguntas
 
 class ClasificadorIntencion:
     def __init__(self, rutas_modelo=["modelo_periodo.joblib","modelo_estadistica.joblib"]): 
